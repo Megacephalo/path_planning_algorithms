01ac8cf4f3f24643c1c82d5f52971d0cc88fb6d6
Megacephalo <charly.charlongo@gmail.com>
2021-03-28 09:09:04 -0400

    Move the trajectory extraction procedure and related data structure to parent class to be shared among all its children classes.

--- include/path_planning_algorithms/path_planner_base.h
+++ include/path_planning_algorithms/path_planner_base.h
@@ -13,13 +13,20 @@ class Path_Planner {
   	Path_Planner(const Grid& grid) ;
   	void plan(const GridLocation& start, const GridLocation& goal) ;
   	virtual void plan(const size_t& startRow, const size_t& startCol, const size_t& endRow, const size_t& endCol) = 0 ;
-  
   	Grid get_grid() const ;
+
   protected:
   	Grid grid_ ;
   	std::vector<GridLocation> directions_ ;
   	std::vector<GridLocation> trajectory_ ;
+
+  	/* (the node, its predecessor) */
+  	std::unordered_map< GridLocation, GridLocation, GridLocation::Hasher > cameFrom_ ;
+
+    void extract_trajectory(const GridLocation& start, const GridLocation& goal) ;
+    bool not_in_cameFrom(const GridLocation& cell) const ;
 } ; /* End of class */
+
 
 } /* End of namespace */
 
--- src/path_planner_base.cpp
+++ src/path_planner_base.cpp
@@ -23,9 +23,43 @@ Path_Planner::plan(const GridLocation& start, const GridLocation& goal) {
 	plan(start.x, start.y, goal.x, goal.y) ;
 } /* End of plan */
 
+void Path_Planner::extract_trajectory(const GridLocation& start, const GridLocation& goal) {
+	if (cameFrom_.empty()) {
+		std::cerr << "The registered steps to be backtracked are missing. No trajectory is extracted." ;
+		return ;
+	}
+
+	trajectory_.push_back(goal) ;
+
+	auto backtrackIt = cameFrom_.find(goal) ;
+	if (backtrackIt == cameFrom_.end()) {
+		std::cerr << "Apparently there is no way to reach the goal. No trajectory extracted.\n" ;
+		return ;
+	}
+
+	while( backtrackIt != cameFrom_.find(start) ) {
+		auto predecessor = backtrackIt->second ;
+
+		if (predecessor == backtrackIt->first) continue ;
+
+		trajectory_.push_back(predecessor) ;
+
+		backtrackIt = cameFrom_.find(predecessor) ;
+	}
+
+	std::reverse(trajectory_.begin(), trajectory_.end()) ;
+
+	grid_.update_trajectory(trajectory_) ;
+} /* End of extract_trajectory */
+
 Grid
 Path_Planner::get_grid() const {
 	return grid_ ;
 } /* End of get_grid */
+
+bool
+Path_Planner::not_in_cameFrom(const GridLocation& cell) const {
+	return (cameFrom_.find(cell) == cameFrom_.end()) ;
+} /* End of not_in_cameFrom */
 
 } /* End of namespace */
